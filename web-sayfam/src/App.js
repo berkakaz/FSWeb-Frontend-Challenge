89b9c453e65e47e899410df3c21d16fc10245743
Berk Akaz <122451877+berkakaz@users.noreply.github.com>
2023-04-12 14:48:50 +0300

    güncelleme 5

--- web-sayfam/src/App.js
+++ web-sayfam/src/App.js
@@ -1,15 +1,39 @@
+import { useEffect } from "react";
+import { ToastContainer,toast } from "react-toastify";
+
 import Header from "./components/Headers";
-import "./App.css";
 import Skills from "./components/Skillss";
 import Profile from "./components/Profiles";
 import Projects from "./components/Projectss";
+
+import "./App.css";
+import 'react-toastify/dist/ReactToastify.css';
+
+
 function App() {
+
+  useEffect ( ()=> {
+    toast(" 🚀 Projeme Hoş Geldin!", {
+      position: "top-right",
+autoClose: 3000,
+hideProgressBar: false,
+closeOnClick: true,
+pauseOnHover: true,
+draggable: true,
+progress: undefined,
+theme: "light",
+className: 'toast-message',
+
+      })
+  }, []);
+
   return (
     <div className="App">
       <Header />
       <Skills />
       <Profile />
       <Projects />
+      <ToastContainer />
     
     </div>
   );
